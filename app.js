b3d0c1fafa8db6edea55c9e74964c63a4811b57d
Noah Rijkaard <noahrijkaard@gmail.com>
2022-08-29 23:15:30 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+function add(){
+    return 0;
+}
+
+function subtract(){
+    return 0;
+}
+
+function multiply(){
+    return 0;
+}
+
+function divide(){
+    return 0;
+}
+
+function operate(operator, num1, num2){
+
+}
