5626e38cc404f5587308f1da8bfebe8894b8f234
Noah Rijkaard <noahrijkaard@gmail.com>
2022-08-31 00:10:37 +0800

    Change format of index page

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
-function add(){
-    return 0;
+let screenValue = 0;
+let firstOperand = null;
+let secondOperand = null;
+let operator = null;
+let result = null;
+
+let buttons = document.querySelectorAll('button');
+
+
+function updateScreen(){
+    let screen = document.getElementById('screen');
+    screen.innerText = screenValue;
+    if(screen.length > 10){
+        screen.innerText = screen.substring(0, 10);
+    }
 }
 
-function subtract(){
-    return 0;
+updateScreen();
+
+function inputOperand(value){
+    if(operator == null)
+    {
+        screenValue = value;
+    } else {
+        screenValue += value;
+    }
 }
 
-function multiply(){
-    return 0;
+function inputOperator(op){
+    if(op == '+' || op == '-' || op == '*' || op == '/'){
+        operator = op;
+        firstOperand = screenValue;
+        screenValue = 0;
+        updateScreen();
+    }
 }
 
-function divide(){
-    return 0;
-}
-
+// does the actual operations
 function operate(operator, num1, num2){
-
+    if(operator == '+'){
+        return num1 + num2;
+    } else if(operator == '-'){
+        return num1 - num2;
+    } else if(operator == '*'){
+        return num1 * num2;
+    } else if(operator == '/'){
+        if(num1==0 || num2 == 0){
+            return "Nope"
+        } else {
+            return num1 / num2;
+        }
+    } 
 }
