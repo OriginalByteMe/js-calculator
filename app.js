01c669928be84c1f7744ca33979f5677c6b01633
Noah Rijkaard <noahrijkaard@gmail.com>
2022-08-31 18:31:40 +0800

    Fixed initial implementation

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-let screenValue = 0;
+let screenValue = '0';
 let firstOperand = null;
 let secondOperand = null;
-let operator = null;
+let firstOperator = null;
+let secondOperator = null;
 let result = null;
 
-let buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('button');
 
 
 function updateScreen(){
@@ -14,40 +15,132 @@ function updateScreen(){
         screen.innerText = screen.substring(0, 10);
     }
 }
-
 updateScreen();
 
+
+function buttonClicks(){
+    for(let i = 0; i < buttons.length; i++){
+        buttons[i].addEventListener('click', function(){
+            let buttonValue = buttons[i].value;
+            if(buttons[i].classList.contains('operand')){
+                inputOperand(buttonValue);
+                updateScreen();
+            } else if(buttons[i].classList.contains('operator')){
+                inputOperator(buttonValue);
+                updateScreen();
+            } else if(buttons[i].classList.contains('equals')){
+                inputEquals();
+                updateScreen();
+            } else if(buttons[i].classList.contains('decimal')){
+                inputDecimal(buttonValue);
+                updateScreen();
+            } else if(buttons[i].classList.contains('clear')){
+                clearScreen();
+                updateScreen();
+            }
+        } 
+    )}
+}
+buttonClicks();
+
+
 function inputOperand(value){
-    if(operator == null)
+    if(firstOperator == null)
     {
-        screenValue = value;
+        if(screenValue === '0' || screenValue === 0){
+            screenValue = value;
+        } else if(screenValue === firstOperand){
+            screenValue = value;
+        } else {
+            screenValue += value;
+        }
     } else {
-        screenValue += value;
+        if(screenValue === firstOperand){
+            screenValue = value; // Screen value will hold second operand till it is ready to be operated on
+        } else {
+            screenValue += value;
+        }
     }
 }
 
 function inputOperator(op){
-    if(op == '+' || op == '-' || op == '*' || op == '/'){
-        operator = op;
+    if(firstOperator != null && secondOperator === null){
+        // 4th click, now have to calculate the result of operand 1 & 2 with operator 1
+        secondOperator = op;
+        secondOperand = screenValue; // Assign the second operand the screen value as it was holding it in place
+        result = operate(firstOperator, Number(firstOperand), Number(secondOperand));
+        screenValue = roundAccurately(result, 10).toString();
+        firstOperand = screenValue; // Rounded result is stored in first operand
+        result = null;
+    } else if(firstOperator != null && secondOperator != null){
+        // 5th click, now have to calculate the result of operand 1 & 2 with operator 2
+        secondOperand = screenValue;
+        result = operate(secondOperator, Number(firstOperand), Number(secondOperand));
+        secondOperator = op;
+        screenValue = roundAccurately(result, 10).toString();
         firstOperand = screenValue;
-        screenValue = 0;
-        updateScreen();
+        result = null;
+    } else {
+        // 2nd click the first operator is stored in firstOperator
+        firstOperator = op;
+        firstOperand = screenValue;
     }
 }
 
+function inputEquals(){
+    if(firstOperator === null){
+        screenValue = screenValue;
+    } else {
+        secondOperand = screenValue;
+        result = operate(firstOperator, Number(firstOperand), Number(secondOperand));
+        console.log(result);
+        if(result == "Nope"){
+            screenValue = "Nope";
+        } else {
+            screenValue = roundAccurately(result, 10).toString();
+            firstOperand = screenValue;
+            firstOperator = null;
+            secondOperator = null;
+            secondOperand = null;
+            result = null;
+        }
+    }
+}
+
+function inputDecimal(dot){
+    if(!screenValue.includes('.')){
+        screenValue += dot;
+    }
+}
+
+function clearScreen(){
+    screenValue = 0;
+    firstOperand = null;
+    secondOperand = null;
+    firstOperator = null;
+    secondOperator = null;
+    result = null;
+}
+        
 // does the actual operations
 function operate(operator, num1, num2){
-    if(operator == '+'){
+    console.log(operator, num1, num2);
+    if(operator === '+'){
         return num1 + num2;
-    } else if(operator == '-'){
+    } else if(operator === '-'){
         return num1 - num2;
-    } else if(operator == '*'){
+    } else if(operator === '*'){
         return num1 * num2;
-    } else if(operator == '/'){
-        if(num1==0 || num2 == 0){
+    } else if(operator === '/'){
+        if(num1===0 || num2 === 0){
             return "Nope"
         } else {
             return num1 / num2;
         }
     } 
+}
+
+
+function roundAccurately(num, places) {
+    return parseFloat(Math.round(num + 'e' + places) + 'e-' + places);
 }
